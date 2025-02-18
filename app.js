17a18607bf45cbb3cc076ce0edc7c43a7744f694
AndreasB09 <andreasb09.kodehode@gmail.com>
2025-02-18 01:44:52 +0100

    quiz section logic

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ document.querySelectorAll(".card").forEach(card => {
     card.textContent = "";
 })
 
+//quiz and results
+const startBtn = document.getElementById("start-quiz");
+const optionsContainer = document.getElementById("options-container");
+const questionDisplay = document.getElementById("question-display");
+const bars = document.querySelectorAll(".bar");
+
+startBtn.addEventListener("click", () => {
+    questionDisplay.textContent = "Pick an option";
+    optionsContainer.style.display = "block";
+    startBtn.style.display ="none";
+});
+
+optionsContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("option")) {
+        questionDisplay.textContent = "Your Love Language Distribution:";
+        optionsContainer.style.display = "none";
+
+        bars.forEach(bar => {
+            let randomHeight = Math.floor(Math.random() * 100) + "%";
+            bar.style.height = randomHeight;
+        })
+    }
+})
 
 //Light/Dark mode
 const themeSwitch = document.getElementById("theme-switch");
